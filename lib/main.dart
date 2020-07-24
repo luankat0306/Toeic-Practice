a5e23102f62a36a801143a2feee1d9f03ccc5802
luan <votriluan99@gmail.com>
2020-07-24 18:33:30 +0700

    luan

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Chuyên Ngành CNTT"),
-          centerTitle: true,
-          backgroundColor: Colors.lightBlue,
-        ),
-        body:Container(
-          padding: const EdgeInsets.only(top: 40),
-          child: Column(
-            children: <Widget>[
-              imageWord,
-              textWord,
-            ],
+      debugShowCheckedModeBanner: false,
+      title: "Toeic Practice",
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: <Widget>[
+          ClipPath(
+            clipper: OvalBottomBorderClipper(),
+            child: Container(
+              height: 300,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                      Color(0xFF396AFC),
+                      Color(0xFF1948FF),
+                    ],
+                ),
+                image: DecorationImage(
+                  image: AssetImage("assets/n1003969.png"),
+                  alignment: Alignment.topCenter,
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.fromLTRB(20, 10, 20, 100),
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    height: 60,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(50),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          spreadRadius: 3,
+                          blurRadius: 5,
+                          offset: Offset(0, 5), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.search,
+                        ),
+                        Center(
+                          child: Text(
+                            "Tra từ điển",
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontWeight: FontWeight.w500
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
-  Widget imageWord = Container(
-    margin: const EdgeInsets.all(25),
-    decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-        boxShadow: [BoxShadow(
-          color: Colors.grey.withOpacity(0.5),
-          spreadRadius: 5,
-          blurRadius: 7,
-          offset: Offset(0, 3)),
-        ],
-    ),
-    child: Image.asset(
-      "assets/operating_system.jpg",
-      width: 400,
-      height: 200),
-  );
-  Widget textWord = Container(
-    decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(20),
-    ),
-    child:
-        Center(
-          child: Container(
-            padding: const EdgeInsets.only(top: 30),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  "Operating System (n)",
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.w700,
-                    color: Colors.grey[800],
-                    shadows: [
-                      Shadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          blurRadius: 5,
-                          offset: Offset(4, 4)),
-                    ],
-                  ),
-                ),
-                Text(
-                  "Hệ Điều Hành",
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.grey[600],
-                  ),
-                ),
-              ],
-            ),
-          ),
-    ),
-  );
 }
+
+
